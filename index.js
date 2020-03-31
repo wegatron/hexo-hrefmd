956bdcc72b8bad5ad2554332cb9f5230b9ab0743
wegatron <wegatron@gmail.com>
2020-03-31 14:11:07 +0800

    update for convert markdown link to hexo link

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 'use strict';
 
 var hexo = hexo || {};
-
-
+// modify for change markdown link to hexo link
 hexo.extend.filter.register('before_post_render', function (data) {
-    data.content=data.content.replace(/\.md/g,'.html');
+  data.content = data.content.replace(
+    /\[(.*)\]\((.*)\)/g,
+    (match, text, url, offset, string) => {
+      if (/^\.\/(.*)/.test(url)) {
+        const asset = url.replace(/\..*\/([\w ]*).md/g, '$1');
+        return '{% post_link '+asset+' \''+text+' \'%}';
+      }
+      return string;
+    },
+  );
 });
